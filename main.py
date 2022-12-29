e5dc333425a28500b5fde5662695fd5f1a21353c
iqthegoat <chetahlife36@gmail.com>
2022-12-29 14:20:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI,Path
 from pydantic import BaseModel
 from typing import Optional
 from datetime import date, timedelta
+import dateutil.relativedelta
 import datetime
-import hashlib,dbinfo,mysql.connector,platform
+import hashlib,dbinfo,mysql.connector,platform,uvicorn
+
 app = FastAPI()
+
 
 
 def get_hwid():
@@ -22,26 +25,39 @@ def get_hwid():
     # Return the HWID
     return hwid
 
-def Register(end_time,key,email,username, password,HWID):
+def Register(key,email,username, password,HWID):
     try:
         """
         Inserts a new record into the Users table with the given username and password
         """
-        if '@gmail' or "@yahoo" in email:
+        if '@gmail' in email or "@yahoo" in email:
             # Hash the password for storage in the database
             hashed_password = HASH(password)
             hwid = HWID
             table = "regkeys"
-            query = f"Select * from {table} WHERE regkey = %s"
+            query = f"Select subtype from {table} WHERE regkey = %s"
             cr.execute(query, (key,))
             results = cr.fetchall()
             if len(results) == 0:
                 return "WRONG KEY OR UNREGISTERED HWID"
                 
             else:
-            
-                values = (username, hashed_password, str(hwid), str(email),end_time)
-                query = f"INSERT INTO {Table} (username, password,HWID,email,End_date) VALUES (%s, %s , %s, %s,%s)"
+                sub = results[0][0]
+                if sub == 'month':
+                    now = datetime.datetime.now()
+                    # Add 30 days to the current date
+                    End_time = now + dateutil.relativedelta.relativedelta(days=31)
+                elif sub == 'week':
+                    now = datetime.datetime.now()
+                    # Add 30 days to the current date
+                    End_time = now + dateutil.relativedelta.relativedelta(days=8)
+                elif sub == "lifetime":
+                    now = datetime.datetime.now()
+                    # Add 30 days to the current date
+                    End_time = now + dateutil.relativedelta.relativedelta(days=5000)
+
+                values = (username, hashed_password, str(hwid), str(email),str(End_time))
+                query = f"INSERT INTO {Table} (username, password,HWID,email,End_date) VALUES (%s, %s, %s, %s, %s)"
                 cr.execute(query, values)
                 cnx.commit()
                 key_column = "regkey"
@@ -54,6 +70,7 @@ def Register(end_time,key,email,username, password,HWID):
             return "IT MUST BE A GMAIL OR YAHOO EMAIL"
     except Exception as e:
         return e 
+    
 
 import secrets
 def gen_key():
@@ -61,13 +78,13 @@ def gen_key():
     key = secrets.token_urlsafe(32)
     return key
 
-def subkey_To_db():
+def subkey_To_db(subtype):
     key = gen_key()
     if key == None:
         return "ERORR"
     else:
-        query = f"INSERT INTO regkeys (regkey) VALUES(%s)"
-        cr.execute(query,(key,))
+        query = f"INSERT INTO regkeys (regkey,subtype) VALUES(%s,%s)"
+        cr.execute(query,(key,subtype))
         cnx.commit()
         return key
 
@@ -103,7 +120,6 @@ def login(username, password,HWID):
         return False
 
 
-
 cnx = mysql.connector.connect(
     host=dbinfo.host,
     user=dbinfo.user,
@@ -137,31 +153,24 @@ def home():
 
 @app.post("/register")
 def register(Data:REG):
-    if Data.End_time == "month":
-        today = date.today()
-        End_time = today.replace(day=1) + timedelta(days=32)
-    elif Data.End_time == "week":
-        now = datetime.datetime.now()
-        End_time = now + datetime.timedelta(weeks=1)
-    elif Data.End_time == "lifetime":
-        End_time = "LIFETIME"
-    resp = Register(key=Data.key,email=Data.email, username=Data.username, password=Data.password,HWID=Data.HWID,end_time=End_time)
-    return {"Status":resp}
+    resp = Register(key=Data.key,email=Data.email, username=Data.username, password=Data.password,HWID=Data.HWID)
+    return {"status":resp}
 
 @app.post("/login")
 def Login(Data:REG):
     resp = login(username=Data.username,password=Data.password,HWID=Data.HWID)
-    return {"Status":resp}
+    return {"status":resp}
 
 @app.post("/resethwid")
 def Login(Data:REG):
     resp = RESETHWID(email=Data.email,password=Data.password,HWID=Data.HWID)
-    return {"Status":resp}
+    return {"status":resp}
 
-@app.get("/genkey/{key}") 
-def gen(key:str):
+@app.get("/genkey/{key}/{type_sub}") 
+def gen(key:str,type_sub:str):
     if key == "2z0d6c4GUs1aqnplLBVAHMg9k2uwfGvCATRyWBtY1pXaBrrFjJLkdb6ogdJZz27dVl79D3oRWMmeZI2uRmm6rRBJ3JtBvsD3bSmgb5Z7ZXW6NX8D2dta97qsGiSFyTwJN8OPfopIryfhBPjk1m0jldE3mTkZ4fWRTBX0f6Qp3VxICULx9iTJeXer52g8sg2S2K9hrEcH51QnCfMOqUQ7UofsCXU89tixYnL5lr0l8RvTXrCf3N2DUpamLaXX1GIf":
-        key = subkey_To_db()
+        key = subkey_To_db(type_sub)
         return{"GENERATED":key}
 
 
+uvicorn.run(app)
